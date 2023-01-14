fc454a404d691b740795eb10e05273b7b8536896
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2023-01-14 13:40:32 -0700

    baseline clean

--- layouts/public/menu.tsx
+++ layouts/public/menu.tsx
@@ -1,7 +1,28 @@
+import Link from "next/link";
+//
 import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 //
 import { useDisplayContext } from "@/context/display";
+
+const routes = [
+  {
+    label: "Home",
+    path: "/",
+  },
+  {
+    label: "Upcoming Events",
+    path: "/upcoming",
+  },
+  {
+    label: "Recordings",
+    path: "/recordings",
+  },
+  {
+    label: "Auth",
+    path: "/auth",
+  },
+];
 
 const PublicMenu = () => {
   const { showMenu, setShowMenu } = useDisplayContext();
@@ -10,29 +31,57 @@ const PublicMenu = () => {
       <Dialog as="div" className="relative z-20" onClose={setShowMenu}>
         <Transition.Child
           as={Fragment}
-          enter="transition duration-300 ease-out"
+          enter="transition duration-700 ease-out"
           enterFrom="opacity-0"
           enterTo="opacity-100"
-          leave="transition duration-300 ease-in"
+          leave="transition duration-500 ease-in"
           leaveFrom="opacity-100"
           leaveTo="opacity-0"
         >
-          <div className="fixed inset-0 bg-black bg-opacity-10 backdrop-blur" />
+          <div className="fixed inset-0 bg-black bg-opacity-10 backdrop-blur">
+            <div className="flex h-16 w-full items-center justify-between px-4 md:px-6">
+              <span />
+
+              <button>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1}
+                  stroke="white"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M6 18L18 6M6 6l12 12"
+                  />
+                </svg>
+              </button>
+            </div>
+          </div>
         </Transition.Child>
         <Transition.Child
           as={Fragment}
-          enter="transition duration-300 ease-out"
+          enter="transition duration-700 ease-out"
           enterFrom="scale-0"
           enterTo="scale-100"
-          leave="transition duration-300 ease-in"
+          leave="transition duration-500 ease-in"
           leaveFrom="scale-100"
           leaveTo="scale-0"
         >
           <div className="fixed inset-0 grid transform place-content-center">
             <Dialog.Panel>
-              <div className="text-white">
-                <button onClick={() => setShowMenu(false)}>Close</button>
-                <p>Menu</p>
+              <div className="grid gap-4 text-center text-white">
+                {routes.map((route, i) => (
+                  <Link
+                    key={i}
+                    href={route.path}
+                    onClick={() => setShowMenu(false)}
+                  >
+                    {route.label}
+                  </Link>
+                ))}
               </div>
             </Dialog.Panel>
           </div>
--- layouts/public/topbar.tsx
+++ layouts/public/topbar.tsx
@@ -7,11 +7,22 @@ const PublicTopbar = () => {
     <div className="fixed z-10 flex h-16 w-full items-center justify-between bg-black bg-opacity-50 px-4 md:px-6">
       <span />
 
-      <span className="grid h-6 w-6" onClick={() => setShowMenu(true)}>
-        <span className="h-0.5 w-full bg-white bg-opacity-90" />
-        <span className="h-0.5 w-full bg-white bg-opacity-90" />
-        <span className="h-0.5 w-full bg-white bg-opacity-90" />
-      </span>
+      <button onClick={() => setShowMenu(true)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1}
+          stroke="currentColor"
+          className="h-6 w-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+      </button>
     </div>
   );
 };
