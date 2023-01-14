c6c6adf207b7737c202b75b5421109086910d3c6
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2023-01-13 20:32:33 -0700

    come on. please

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -5,6 +5,7 @@ const inter = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   subsets: ["latin"],
   variable: "--font-family-sans",
+  display: "swap",
 });
 
 const DefaultLayout: React.FC<WithChildren> = ({ children }) => {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import { api } from "@/lib/api";
 
 export default function Home() {
-  const test = api.user.getAll.useQuery();
-  console.log(test);
+  const { data } = api.user.getAll.useQuery();
+
+  console.log(data);
+
   return (
     <main className="flex h-full w-full flex-col items-center justify-center text-3xl font-thin">
       <h1>Michael Weitzenhoffer</h1>
