5be67d941897b155e2c7b3f5131a0bab6806c0d8
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2023-01-13 21:26:50 -0700

    trying to fix prisma for production

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,17 +1,5 @@
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // eslint-disable-next-line no-var
-  var prisma: PrismaClient;
-}
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!globalThis.prisma) {
-    globalThis.prisma = new PrismaClient();
-  }
-  prisma = globalThis.prisma;
-}
+const prisma = new PrismaClient();
 
 export default prisma;
