e39d41f9025a9184397b2d1b6943faa14fd0b041
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2023-01-13 21:22:04 -0700

    hmmm. prisma is not defined in production build

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,7 +4,8 @@ import { type NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 //
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "@/lib/prisma";
+// import prisma from "@/lib/prisma";
+import prisma from "../../../lib/prisma";
 
 /*
  *
