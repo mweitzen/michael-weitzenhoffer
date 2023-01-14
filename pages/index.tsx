3fc90c0438bc0b0d354c633c705e7ce1b6857edc
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2023-01-13 20:25:43 -0700

    come on. please

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
+import { api } from "@/lib/api";
+
 export default function Home() {
+  const test = api.user.getAll.useQuery();
+  console.log(test);
   return (
     <main className="flex h-full w-full flex-col items-center justify-center text-3xl font-thin">
       <h1>Michael Weitzenhoffer</h1>
