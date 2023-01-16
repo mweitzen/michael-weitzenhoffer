5e8e34c22146c15b4b830735ef94affe63a95342
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2023-01-15 18:41:58 -0700

    why does it fail on vercel

--- lib/api/routers/event.ts
+++ lib/api/routers/event.ts
@@ -6,7 +6,7 @@ export const eventRouter = createTRPCRouter({
    * @summary GET ALL
    */
   getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.event.findMany();
+    // return ctx.prisma.event.findMany();
   }),
 
   /**
@@ -15,10 +15,10 @@ export const eventRouter = createTRPCRouter({
   getById: publicProcedure
     .input(z.object({ eventId: z.string() }))
     .query(({ ctx, input }) => {
-      return ctx.prisma.event.findUnique({
-        where: {
-          id: input.eventId,
-        },
-      });
+      // return ctx.prisma.event.findUnique({
+      //   where: {
+      //     id: input.eventId,
+      //   },
+      // });
     }),
 });
