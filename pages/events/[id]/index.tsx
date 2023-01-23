726ce313835ceb3a89f91e80bae98bae86233aad
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2023-01-22 23:02:33 -0700

    like i said. not liking it

--- pages/events/[id]/index.tsx
+++ pages/events/[id]/index.tsx
@@ -1,5 +1,6 @@
 import { GetStaticPropsContext, InferGetStaticPropsType } from "next";
 //
+import { api } from "@/lib/api";
 import prisma from "@/lib/prisma";
 import { formatDateSimple, formatTimeSimple } from "@/lib/formatters/dates";
 //
@@ -9,60 +10,67 @@ import ActionBar from "@/components/action-bar";
 import ArrowUpRightOnSquare from "@/icons/arrow-up-right-on-square";
 import CalendarDays from "@/icons/calendar-days";
 
-export default function EventDetailPage({
-  event,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
+export default function EventDetailPage(
+  props: InferGetStaticPropsType<typeof getStaticProps>
+) {
+  const { data: event, isLoading } = api.events.getById.useQuery({
+    eventId: props.id || "",
+  });
+
   return (
     <Page header="Event Detail Page" seoTitle="Event Detail">
-      <div className="space-y-4 px-4 pb-8">
-        <h2 className="text-xl">{event.name}</h2>
-        <div>
-          <p className="text-sm">Location:</p>
-          <p>{event.location.name}</p>
-          <p>{event.stage?.name}</p>
-          <p>{event.location.address.complete}</p>
-        </div>
-        <div>
-          <p className="text-sm">Date</p>
-          <p>{formatDateSimple(event.timeStart || new Date())}</p>
-        </div>
-        <div>
-          <p className="text-sm">Time</p>
-          <p>{`${formatTimeSimple(
-            event.timeStart || new Date()
-          )} - ${formatTimeSimple(event.timeEnd || new Date())}`}</p>
-        </div>
-        <p>{event.description}</p>
-      </div>
-
-      <ActionBar
-        actions={[
-          {
-            type: "link",
-            internal: false,
-            href: event.location.website || "",
-            target: "_blank",
-            children: (
-              <>
-                <ArrowUpRightOnSquare />
-                Visit
-              </>
-            ),
-          },
-          {
-            type: "link",
-            internal: false,
-            href: event.rsvpLink || "",
-            target: "_blank",
-            children: (
-              <>
-                <CalendarDays />
-                Reservation
-              </>
-            ),
-          },
-        ]}
-      />
+      {isLoading || !event ? null : (
+        <>
+          <div className="space-y-4 px-4">
+            <h2 className="text-xl">{event.name}</h2>
+            <div>
+              <p className="text-sm">Location:</p>
+              <p>{event.location.name}</p>
+              <p>{event.stage?.name}</p>
+              <p>{event.location.address.complete}</p>
+            </div>
+            <div>
+              <p className="text-sm">Date</p>
+              <p>{formatDateSimple(event.timeStart || new Date())}</p>
+            </div>
+            <div>
+              <p className="text-sm">Time</p>
+              <p>{`${formatTimeSimple(
+                event.timeStart || new Date()
+              )} - ${formatTimeSimple(event.timeEnd || new Date())}`}</p>
+            </div>
+            <p>{event.description}</p>
+          </div>
+          <ActionBar
+            actions={[
+              {
+                type: "link",
+                internal: false,
+                href: event.location.website || "",
+                target: "_blank",
+                children: (
+                  <>
+                    <ArrowUpRightOnSquare />
+                    Visit
+                  </>
+                ),
+              },
+              {
+                type: "link",
+                internal: false,
+                href: event.rsvpLink || "",
+                target: "_blank",
+                children: (
+                  <>
+                    <CalendarDays />
+                    Reservation
+                  </>
+                ),
+              },
+            ]}
+          />
+        </>
+      )}
     </Page>
   );
 }
@@ -85,31 +93,9 @@ export async function getStaticProps(
     };
   }
 
-  const event = await prisma.event.findUnique({
-    where: {
-      id: context.params.id,
-    },
-    include: {
-      location: {
-        include: {
-          address: true,
-        },
-      },
-      performingArtist: true,
-      performingGroup: true,
-      stage: true,
-    },
-  });
-
-  if (!event) {
-    return {
-      notFound: true,
-    };
-  }
-
   return {
     props: {
-      event,
+      id: context.params.id,
     },
   };
 }
