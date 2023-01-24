a12f4f6ba359d98bcb753391dc524c74abeaf342
Michael Weitzenhoffer <mweitzenhoffer@gmail.com>
2023-01-24 14:41:35 -0700

    lol now event ssg

--- pages/events/[id]/index.tsx
+++ pages/events/[id]/index.tsx
@@ -67,73 +67,71 @@ export async function getStaticProps(
  * PAGE
  *
  */
-export default function EventDetailPage(props: StaticProps) {
-  const { data: event, isLoading } = api.events.getById.useQuery({
-    eventId: props.id,
-  });
+export default function EventDetailPage({ event }: StaticProps) {
+  // const { data: event, isLoading } = api.events.getById.useQuery({
+  //   eventId: props.id,
+  // });
 
   return (
     <Page header="Event Detail Page" seoTitle="Event Detail">
-      {isLoading || !event ? null : (
-        <>
-          <div className="space-y-4 px-4 pb-8">
-            <h2 className="text-xl">{event.name}</h2>
-            <div>
-              <p className="text-sm">Location:</p>
-              <p>{event.location.name}</p>
-              <p>{event.stage?.name}</p>
-              <p>{event.location.address.complete}</p>
-            </div>
-            <div>
-              <p className="text-sm">Date</p>
-              <p>
-                {event.timeStart
-                  ? formatDateSimple(event.timeStart)
-                  : "Not scheduled."}
-              </p>
-            </div>
-            <div>
-              <p className="text-sm">Time</p>
-              <p>
-                {event.timeStart
-                  ? formatTimeSimple(event.timeStart)
-                  : "No time set."}
-                {event.timeEnd ? ` - ${formatTimeSimple(event.timeEnd)}` : ""}
-              </p>
-            </div>
-            <p>{event.description}</p>
+      <>
+        <div className="space-y-4 px-4 pb-8">
+          <h2 className="text-xl">{event.name}</h2>
+          <div>
+            <p className="text-sm">Location:</p>
+            <p>{event.location.name}</p>
+            <p>{event.stage?.name}</p>
+            <p>{event.location.address.complete}</p>
           </div>
+          <div>
+            <p className="text-sm">Date</p>
+            <p>
+              {event.timeStart
+                ? formatDateSimple(event.timeStart)
+                : "Not scheduled."}
+            </p>
+          </div>
+          <div>
+            <p className="text-sm">Time</p>
+            <p>
+              {event.timeStart
+                ? formatTimeSimple(event.timeStart)
+                : "No time set."}
+              {event.timeEnd ? ` - ${formatTimeSimple(event.timeEnd)}` : ""}
+            </p>
+          </div>
+          <p>{event.description}</p>
+        </div>
 
-          <ActionBar
-            actions={[
-              {
-                type: "link",
-                internal: false,
-                href: event.location.website || "",
-                target: "_blank",
-                children: (
-                  <>
-                    <ArrowUpRightOnSquare />
-                    Visit
-                  </>
-                ),
-              },
-              {
-                type: "link",
-                internal: false,
-                href: event.rsvpLink || "",
-                target: "_blank",
-                children: (
-                  <>
-                    <CalendarDays />
-                    Reservation
-                  </>
-                ),
-              },
-            ]}
-          />
-        </>
-      )}
+        <ActionBar
+          actions={[
+            {
+              type: "link",
+              internal: false,
+              href: event.location.website || "",
+              target: "_blank",
+              children: (
+                <>
+                  <ArrowUpRightOnSquare />
+                  Visit
+                </>
+              ),
+            },
+            {
+              type: "link",
+              internal: false,
+              href: event.rsvpLink || "",
+              target: "_blank",
+              children: (
+                <>
+                  <CalendarDays />
+                  Reservation
+                </>
+              ),
+            },
+          ]}
+        />
+      </>
     </Page>
   );
 }
